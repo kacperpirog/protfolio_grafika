47d1af3974050614dda94f93a1dcd29465b14e73
Kacper Pirog <kacper.pirog@gmail.com>
2023-03-06 12:15:53 +0100

    styled div exp

--- src/components/experience/StyledExperience.js
+++ src/components/experience/StyledExperience.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const StyledDivExp = styled.div`
   display: flex;
+  flex-wrap: wrap;
   padding: 1rem;
   background-color: #fff;
   box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
